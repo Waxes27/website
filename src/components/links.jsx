8b74d4c66a2284b33ae4376ab2d0c6faed4aae86
ndumasi <ndumasi@student.wethinkcode.co.za>
2021-06-02 23:54:21 +0200

    Perfection

--- src/components/links.jsx
+++ src/components/links.jsx
@@ -5,10 +5,10 @@ const links = () => {
         <div>
             <nav className="navig">
                 <ul className="wholelist">
-                    <li><a href="#">Home</a></li>
+                    {/* <li><a href="#">Home</a></li> */}
                     <li><a href="https://github.com/waxes27" target="_blank" rel="noreferrer">Github</a></li>
                     <li><a href="https://linkedin.com/in/waxes27" target="_blank" rel="noreferrer">LinkedIn</a></li>
-                    <li className="lastlink"><a href="https://github.com/waxes27" target="_blank" rel="noreferrer">Instagram</a></li>
+                    <li className="lastlink"><a href="#" rel="noreferrer">Instagram</a></li>
                 </ul>
             </nav>
         </div>
